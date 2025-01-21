ad151243cea668d88bc07b4af634a6beada142c1
WindanceBoardshop <windance@windance.com>
2025-01-20 16:42:08 -0800

    revised version

--- BarcodeMaker.R
+++ BarcodeMaker.R
@@ -1,22 +1,27 @@
 library(ggplot2)
 library(stringr)
 library(tidyverse)
+library(baRcodeR)
 
 data <- read.csv(paste0('Data/', list.files('Data')))
 
-   
+  data <- data %>% 
+    mutate(brand = str_extract(Title, "^[^\\s]+\\s*"),
+           Title = str_remove(Title, "^[^\\s]+\\s*")
+           )
    
    
 custom_create_PDF( name = 'LibrarikaLabels',
                    Labels = data$Accession.No,
-                   type = 'linear',
-                   alt_text = paste(data$Title,'\n',data$Accession.No),
+                   type = 'linear2',
+                   alt_text = paste(data$brand, "\n", data$Title,'\n','ID:',data$Accession.No),
                    page_width = 2,
-                   page_height = 1.5,
+                   page_height = 2,
                   numcol = 1, 
                    replace_label = TRUE, 
                    numrow = 1,
                    trunc = FALSE,
                   height_margin = .2,
-                  Fsz = 7)
+                  Fsz = 9.5,
+                  denote = "")
 
